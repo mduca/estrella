783366db13844d7a59f44bddc30952d85c60861e
Kevin Smouts (Pro) <kevin.smouts@fabernovel.com>
2015-05-30 16:36:40 -0700

    demo template for controller

--- app/controllers/screening_controller.rb
+++ app/controllers/screening_controller.rb
@@ -1,2 +1,30 @@
 class ScreeningController < ApplicationController
+
+	def question1
+		@question = :question_1
+		@options = ["Yes", "No"]
+		@info = "the info text"
+		render: question_template
+
+		if request.post?
+			if params["answer"] == "yes"
+				redirect_to: question1sub2
+
+				@current_path.question_1 = params["answer"]
+
+				# or final
+				@filter
+				redirect_to: result_page
+			else
+
+			end
+
+		end
+
+
+	end
+
+
+
+
 end
