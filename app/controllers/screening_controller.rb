383d2092df72a0451f31abb6f9633e30daf26a6d
Kevin Smouts (Pro) <kevin.smouts@fabernovel.com>
2015-05-30 23:18:07 -0700

    DECISION TREE IS DONE

--- app/controllers/screening_controller.rb
+++ app/controllers/screening_controller.rb
@@ -75,11 +75,14 @@ class ScreeningController < ApplicationController
 			render "screening/question"
 
 		else
+			@current_path.question_2 = params["answer"]
+			@current_path.save
+
 			if params["answer"] == "question_2_option_1"
 
 				redirect_to action: :question_2_sub_1
 			else
-				redirect_to :root
+				redirect_to action: :question_3
 			end
 
 		end
@@ -88,20 +91,47 @@ class ScreeningController < ApplicationController
 
 	def question_2_sub_1
 		unless request.post?
-
 			@question = :question_2_sub_1  #locale key to get question string
 			@options = [:question_2_sub_1_option_1, :question_2_sub_1_option_2] #array of keys of options to get answers
 			@info = :question_2_sub_1_info #locale key to get info
 
+			render "screening/question"
+
+		else
+			@current_path.question_2_sub_1 = params["answer"]
+			@current_path.save
+
+			if params["answer"] == "question_2_sub_1_option_1"
+				@current_path.filter_area = "9"
+				@current_path.save
+				redirect_to "/results"
+			else
+				redirect_to action: :question_3
+			end
+
+		end
+
+	end
+
+
+	def question_2_sub_2
+		unless request.post?
+			@question = :question_2_sub_2  #locale key to get question string
+			@options = [:question_2_sub_2_option_1, :question_2_sub_2_option_2] #array of keys of options to get answers
+			@info = :question_2_sub_2_info #locale key to get info
 
 			render "screening/question"
 
 		else
-			if params["answer"] == "question_2_option_1"
+			@current_path.question_2_sub_2 = params["answer"]
+			@current_path.save
 
-				redirect_to action: :question_2_sub_1
+			if params["answer"] == "question_2_sub_2_option_1"
+				@current_path.filter_area = "6"
+				@current_path.save
+				redirect_to "/results"
 			else
-				redirect_to :root
+				redirect_to action: :question_3
 			end
 
 		end
@@ -112,18 +142,22 @@ class ScreeningController < ApplicationController
 	def question_3
 
 		unless request.post?
-			@question = :question_1  #locale key to get question string
-			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
-			@info = :question_1_info #locale key to get info
+			@question = :question_3  #locale key to get question string
+			@options = [:question_3_option_1, :question_3_option_2] #array of keys of options to get answers
+			@info = :question_3_info #locale key to get info
 
 			render "screening/question"
 
 		else
-			if params["answer"] == "question_1_option_1"
+			@current_path.question_3 = params["answer"]
+			@current_path.save
 
-				redirect_to action: :question_1_sub_1
+			if params["answer"] == "question_3_option_1"
+				@current_path.filter_area = "6"
+				@current_path.save
+				redirect_to "/results"
 			else
-				redirect_to :root
+				redirect_to action: :question_4
 			end
 
 		end
@@ -133,18 +167,22 @@ class ScreeningController < ApplicationController
 	def question_4
 
 		unless request.post?
-			@question = :question_1  #locale key to get question string
-			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
-			@info = :question_1_info #locale key to get info
+			@question = :question_4  #locale key to get question string
+			@options = [:question_4_option_1, :question_4_option_2] #array of keys of options to get answers
+			@info = :question_4_info #locale key to get info
 
 			render "screening/question"
 
 		else
-			if params["answer"] == "question_1_option_1"
+			@current_path.question_4 = params["answer"]
+			@current_path.save
 
-				redirect_to action: :question_1_sub_1
+			if params["answer"] == "question_4_option_1"
+				@current_path.filter_service = "4"
+				@current_path.save
+				redirect_to "/results"
 			else
-				redirect_to :root
+				redirect_to action: :question_5
 			end
 
 		end
@@ -156,18 +194,23 @@ class ScreeningController < ApplicationController
 	def question_5
 
 		unless request.post?
-			@question = :question_1  #locale key to get question string
-			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
-			@info = :question_1_info #locale key to get info
+			@question = :question_5  #locale key to get question string
+			@options = [:question_5_option_1, :question_5_option_2] #array of keys of options to get answers
+			@info = :question_5_info #locale key to get info
 
 			render "screening/question"
 
 		else
-			if params["answer"] == "question_1_option_1"
+			@current_path.question_5 = params["answer"]
+			@current_path.save
 
-				redirect_to action: :question_1_sub_1
+			if params["answer"] == "question_5_option_1"
+
+				@current_path.filter_area = "14"
+				@current_path.save
+				redirect_to "/results"
 			else
-				redirect_to :root
+				redirect_to action: :question_6
 			end
 
 		end
@@ -179,18 +222,23 @@ class ScreeningController < ApplicationController
 	def question_6
 
 		unless request.post?
-			@question = :question_1  #locale key to get question string
-			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
-			@info = :question_1_info #locale key to get info
+			@question = :question_6  #locale key to get question string
+			@options = [:question_6_option_1, :question_6_option_2] #array of keys of options to get answers
+			@info = :question_6_info #locale key to get info
 
 			render "screening/question"
 
 		else
-			if params["answer"] == "question_1_option_1"
+			@current_path.question_6 = params["answer"]
+			@current_path.save
 
-				redirect_to action: :question_1_sub_1
+			if params["answer"] == "question_6_option_1"
+
+				@current_path.filter_area = "14,6"
+				@current_path.save
+				redirect_to "/results"
 			else
-				redirect_to :root
+				redirect_to action: :question_7
 			end
 
 		end
@@ -202,18 +250,22 @@ class ScreeningController < ApplicationController
 	def question_7
 
 		unless request.post?
-			@question = :question_1  #locale key to get question string
-			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
-			@info = :question_1_info #locale key to get info
+			@question = :question_7  #locale key to get question string
+			@options = [:question_7_option_1, :question_7_option_2] #array of keys of options to get answers
+			@info = :question_7_info #locale key to get info
 
 			render "screening/question"
 
 		else
-			if params["answer"] == "question_1_option_1"
+			@current_path.question_7 = params["answer"]
+			@current_path.save
 
-				redirect_to action: :question_1_sub_1
+			if params["answer"] == "question_7_option_1"
+				@current_path.filter_area = "14,15"
+				@current_path.save
+				redirect_to "/results"
 			else
-				redirect_to :root
+				redirect_to action: :question_8
 			end
 
 		end
@@ -225,18 +277,22 @@ class ScreeningController < ApplicationController
 	def question_8
 
 		unless request.post?
-			@question = :question_1  #locale key to get question string
-			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
-			@info = :question_1_info #locale key to get info
+			@question = :question_8  #locale key to get question string
+			@options = [:question_8_option_1, :question_8_option_2] #array of keys of options to get answers
+			@info = :question_8_info #locale key to get info
 
 			render "screening/question"
 
 		else
-			if params["answer"] == "question_1_option_1"
+			@current_path.question_8 = params["answer"]
+			@current_path.save
 
-				redirect_to action: :question_1_sub_1
+			if params["answer"] == "question_8_option_1"
+				@current_path.filter_area = "2"
+				@current_path.save
+				redirect_to "/results"
 			else
-				redirect_to :root
+				redirect_to action: :question_9
 			end
 
 		end
@@ -248,40 +304,115 @@ class ScreeningController < ApplicationController
 	def question_9
 
 		unless request.post?
-			@question = :question_1  #locale key to get question string
-			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
-			@info = :question_1_info #locale key to get info
+			@question = :question_9  #locale key to get question string
+			@options = [:question_9_option_1, :question_9_option_2] #array of keys of options to get answers
+			@info = :question_9_info #locale key to get info
 
 			render "screening/question"
 
 		else
-			if params["answer"] == "question_1_option_1"
+			@current_path.question_9 = params["answer"]
+			@current_path.save
 
-				redirect_to action: :question_1_sub_1
+			if params["answer"] == "question_9_option_1"
+
+				redirect_to action: :question_9_sub_1
 			else
-				redirect_to :root
+				redirect_to "/results"
 			end
 
 		end
 	end
 
 
-
-
 	def question_9_sub_1
+		unless request.post?
+			@question = :question_9_sub_1  #locale key to get question string
+			@options = [:question_9_sub_1_option_1, :question_9_sub_1_option_2] #array of keys of options to get answers
+			@info = :question_9_sub_1_info #locale key to get info
 
+			render "screening/question"
+
+		else
+			@current_path.question_9_sub_1 = params["answer"]
+			@current_path.save
+
+			if params["answer"] == "question_9_sub_1_option_1"
+
+				redirect_to action: :question_9_sub_1_sub_2
+			else
+				redirect_to "/results"
+			end
+
+		end
 	end
 
 	def question_9_sub_2
+		unless request.post?
+			@question = :question_9_sub_2  #locale key to get question string
+			@options = [:question_9_sub_2_option_1, :question_9_sub_2_option_2] #array of keys of options to get answers
+			@info = :question_9_sub_2_info #locale key to get info
 
+			render "screening/question"
+
+		else
+			@current_path.question_9_sub_2 = params["answer"]
+			@current_path.save
+
+			if params["answer"] == "question_9_sub_2_option_1"
+
+				redirect_to action: :question_9_sub_2_sub_3
+			else
+				redirect_to "/results"
+			end
+
+		end
 	end
 
 	def question_9_sub_3
+		unless request.post?
+			@question = :question_9_sub_3  #locale key to get question string
+			@options = [:question_9_sub_3_option_1, :question_9_sub_3_option_2] #array of keys of options to get answers
+			@info = :question_9_sub_3_info #locale key to get info
 
+			render "screening/question"
+
+		else
+			@current_path.question_9_sub_3 = params["answer"]
+			@current_path.save
+
+			if params["answer"] == "question_9_sub_3_option_1"
+
+				redirect_to action: :question_9_sub_3_sub_1
+			else
+				redirect_to "/results"
+			end
+
+		end
 	end
 
 	def question_9_sub_4
+		unless request.post?
+			@question = :question_9_sub_4  #locale key to get question string
+			@options = [:question_9_sub_4_option_1, :question_9_sub_4_option_2] #array of keys of options to get answers
+			@info = :question_9_sub_4_info #locale key to get info
 
+			render "screening/question"
+
+		else
+			@current_path.question_9_sub_4 = params["answer"]
+			@current_path.save
+
+			if params["answer"] == "question_9_sub_4_option_1"
+
+				redirect_to "/results"
+			else
+				@current_path.filter_area = "16"
+				@current_path.save
+				redirect_to "/results"
+			end
+
+		end
 	end
 
 
