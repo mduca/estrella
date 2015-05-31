e31b32526ff0dfb602c0782f8847903a176b4e84
Michael Manduca <michael@mandu.ca>
2015-05-30 21:35:21 -0700

    Fillout user screening tree

--- app/controllers/screening_controller.rb
+++ app/controllers/screening_controller.rb
@@ -1,16 +1,14 @@
 class ScreeningController < ApplicationController
 	 layout "guided"
-
+  
 	def question_1
-		unless request.post?
 
+		unless request.post?
 			@question = :question_1  #locale key to get question string
 			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
 			@info = :question_1_info #locale key to get info
 
-
 			render "screening/question"
-
 
 		else
 			if params["answer"] == "question_1_option_1"
@@ -47,38 +45,209 @@ class ScreeningController < ApplicationController
 
 	def question_2
 
+		unless request.post?
+			@question = :question_2  #locale key to get question string
+			@options = [:question_2_option_1, :question_2_option_2] #array of keys of options to get answers
+			@info = :question_2_info #locale key to get info
+
+			render "screening/question"
+
+		else
+			if params["answer"] == "question_2_option_1"
+
+				redirect_to action: :question_2_sub_1
+			else
+				redirect_to :root
+			end
+
+		end
+	end
+
+
 
 	end
 
 	def question_2_sub_1
+		unless request.post?
+
+			@question = :question_2_sub_1  #locale key to get question string
+			@options = [:question_2_sub_1_option_1, :question_2_sub_1_option_2] #array of keys of options to get answers
+			@info = :question_2_sub_1_info #locale key to get info
+
+
+			render "screening/question"
+
+		else
+			if params["answer"] == "question_2_option_1"
+
+				redirect_to action: :question_2_sub_1
+			else
+				redirect_to :root
+			end
+
+		end
 
 	end
 
+
 	def question_3
+
+		unless request.post?
+			@question = :question_1  #locale key to get question string
+			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
+			@info = :question_1_info #locale key to get info
+
+			render "screening/question"
+
+		else
+			if params["answer"] == "question_1_option_1"
+
+				redirect_to action: :question_1_sub_1
+			else
+				redirect_to :root
+			end
+
+		end
+	end
+
 
 	end
 
 	def question_4
 
+		unless request.post?
+			@question = :question_1  #locale key to get question string
+			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
+			@info = :question_1_info #locale key to get info
+
+			render "screening/question"
+
+		else
+			if params["answer"] == "question_1_option_1"
+
+				redirect_to action: :question_1_sub_1
+			else
+				redirect_to :root
+			end
+
+		end
+	end
+
+
 	end
 
 	def question_5
+
+		unless request.post?
+			@question = :question_1  #locale key to get question string
+			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
+			@info = :question_1_info #locale key to get info
+
+			render "screening/question"
+
+		else
+			if params["answer"] == "question_1_option_1"
+
+				redirect_to action: :question_1_sub_1
+			else
+				redirect_to :root
+			end
+
+		end
+	end
+
 
 	end
 
 	def question_6
 
+		unless request.post?
+			@question = :question_1  #locale key to get question string
+			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
+			@info = :question_1_info #locale key to get info
+
+			render "screening/question"
+
+		else
+			if params["answer"] == "question_1_option_1"
+
+				redirect_to action: :question_1_sub_1
+			else
+				redirect_to :root
+			end
+
+		end
+	end
+
+
 	end
 
 	def question_7
+
+		unless request.post?
+			@question = :question_1  #locale key to get question string
+			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
+			@info = :question_1_info #locale key to get info
+
+			render "screening/question"
+
+		else
+			if params["answer"] == "question_1_option_1"
+
+				redirect_to action: :question_1_sub_1
+			else
+				redirect_to :root
+			end
+
+		end
+	end
+
 
 	end
 
 	def question_8
 
+		unless request.post?
+			@question = :question_1  #locale key to get question string
+			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
+			@info = :question_1_info #locale key to get info
+
+			render "screening/question"
+
+		else
+			if params["answer"] == "question_1_option_1"
+
+				redirect_to action: :question_1_sub_1
+			else
+				redirect_to :root
+			end
+
+		end
+	end
+
+
 	end
 
 	def question_9
+
+		unless request.post?
+			@question = :question_1  #locale key to get question string
+			@options = [:question_1_option_1, :question_1_option_2] #array of keys of options to get answers
+			@info = :question_1_info #locale key to get info
+
+			render "screening/question"
+
+		else
+			if params["answer"] == "question_1_option_1"
+
+				redirect_to action: :question_1_sub_1
+			else
+				redirect_to :root
+			end
+
+		end
+	end
+
 
 	end
 
