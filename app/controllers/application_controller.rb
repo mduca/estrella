63125066338024cd7a738eb3d8817d9877b045c3
Felipe <auditor400@gmail.com>
2015-05-31 00:23:55 -0700

    fix bug that prevents locales to be changed once they are set in the session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
  
 	def set_locale
-	  I18n.locale = session[:locale] || params[:locale] || I18n.default_locale
+	  I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
 	end
 
 	private
