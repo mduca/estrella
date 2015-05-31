4667701bcbd9e9ab8c18819f6bc89b1ea644024c
Felipe <auditor400@gmail.com>
2015-05-30 23:59:26 -0700

    fix stupid locale error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+	#  if session[:locale] == nil then
+	#    session[:locale]=0
+	#	else
+	#    session[:locale] +=1 #params[:locale]
+	#	end
   end
 end
